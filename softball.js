d5e0ce50a6e1e1c6ff14a5b3a7bd5ec11529be2a
Ben <ishmandoo@gmail.com>
2015-07-05 15:41:07 -0400

    added player collection and player adding and removing and resorting

--- softball.js
+++ softball.js
@@ -1,23 +1,50 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Players = new Mongo.Collection("players");
+posNames = ['Sit', 'P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'RC', 'LC', 'LF', 'Sit']
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+
+  Template.field.helpers({
+    players: function () {
+      return Players.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.lineup.helpers({
+    players: function () {
+      return Players.find({});
+    },
+    posNames: function(pos) {
+      return posNames[pos];
+    }
+  });
+
+  Template.lineup.events({
+    "submit .new-player": function (event) {
+
+      var name = event.target.text.value;
+
+      Players.insert({
+        name: name,
+        pos: 0,
+        bat: Players.find({}).count()
+      });
+
+      event.target.text.value = "";
+
+      return false;
+    }
+  });
+
+  Template.player.events({
+    "click .delete": function () {
+      Players.remove(this._id);
     }
   });
 }
 
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Sortable.collections = ["players"];
+  })
 }
